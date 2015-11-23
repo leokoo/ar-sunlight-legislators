fb55ccc8b124316844762b4bf0801e76df36e367
Leo Koo <leokoo@gmail.com>
2015-11-23 19:14:23 +0800

    2nd part

--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,36 @@
 require_relative '../app/models/legislator'
-def names
-	puts "testing!"
-	legislator = Legislator.all
-	puts "Please enter a state"
-	state_entry = gets.chomp.upcase
-	puts "\nSenators: "
-	Legislator.find_sen_by_state(state_entry).each { |sen_name|
-		p sen_name
-	}
-	puts "\nRepresentatives: "
-	Legislator.find_rep_by_state(state_entry).each { |rep_name|
-		p rep_name
-	}
+def rep_percentage
+	puts "Please enter a gender to find the percentage"
+	gender_entry = gets.chomp.upcase
+	case gender_entry
+	when "M"
+		p "Male : #{Legislator.where(gender: "M").count.to_f}"
+		p "Total : #{Legislator.all.count.to_f}"
+		p "Percentage : #{(Legislator.where(gender: "M").count.to_f/Legislator.all.count.to_f) * 100}"
+	when "F"
+		p "Female : #{Legislator.where(gender: "F").count}"
+		p "Total : #{Legislator.all.count}"
+		p "Percentage : #{(Legislator.where(gender: "F").count.to_f/Legislator.all.count.to_f) * 100}"
+	else
+		puts "You're just making it up! :p"
+	end
 end
 
-names
-# Legislator.first.name
+rep_percentage
+
+
+
+# def names
+# 	puts "Please enter a state"
+# 	state_entry = gets.chomp.upcase
+# 	puts "\nSenators: "
+# 	Legislator.find_sen_by_state(state_entry).each { |sen_name|
+# 		p sen_name
+# 	}
+# 	puts "\nRepresentatives: "
+# 	Legislator.find_rep_by_state(state_entry).each { |rep_name|
+# 		p rep_name
+# 	}
+# end
+# names
+
